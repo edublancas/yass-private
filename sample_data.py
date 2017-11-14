1b4c7b89cf0f89605a8ae80dc78d59abc25b18f6
Eduardo Blancas Reyes <fkq8@blancas.io>
2017-11-14 17:58:23 -0500

    removes old files from config refactor

--- sample_data.py
+++ sample_data.py
@@ -10,3 +10,5 @@ d = d.reshape(6000000, 49)
 d[:1000, 0:7].tofile('sample_data/sample_1k.bin')
 d[:10000, 0:7].tofile('sample_data/sample_10k.bin')
 d[:100000, 0:7].tofile('sample_data/sample_100k.bin')
+
+d[:, 0:7].tofile('/users/Edu/data/yass/7ch.bin')
