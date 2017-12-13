aab9e676987dc5abaf4cdb9ac55e11f28b263037
Eduardo Blancas Reyes <fkq8@blancas.io>
2017-12-12 22:53:37 -0500

    updates example

--- playground/pipeline.py
+++ playground/pipeline.py
@@ -10,6 +10,7 @@ import logging
 
 import yass
 from yass import preprocess
+from yass.explore import RecordingExplorer
 
 # configure logging module to get useful information
 logging.basicConfig(level=logging.INFO)
@@ -19,21 +20,4 @@ yass.set_config('config/threshold100k.yaml')
 # yass.set_config('config/threshold.yaml')
 
 # run preprocessor
-spike_index, whitened, rotation = preprocess.run()
-
-time, channel = spike_index[1, :]
-
-spike = whitened[time-15:time+16, :]
-
-plt.plot(spike)
-plt.show()
-
-spikes = np.stack([spike, spike])
-spikes.shape
-rotation.shape
-
-# this should be done by the pca matrix function
-rot = np.transpose(rotation)
-sp = np.transpose(spikes)
-
-reduced = np.transpose(np.matmul(rot, sp), (2, 1, 0))
+spike_index, scores = preprocess.run()
