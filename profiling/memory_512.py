41692ca3fd476e6f774a3182bbfb1097d8fa5c56
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-02-28 21:53:08 -0500

    logging

--- profiling/memory_512.py
+++ profiling/memory_512.py
@@ -1,13 +1,10 @@
 from yass import pipeline
 from memory_profiler import profile
-import logging
-
-logging.basicConfig(level=logging.DEBUG)
 
 
 @profile
 def fn():
-    pipeline.run('512.yaml', clean=True)
+    pipeline.run('512.yaml', clean=True, logger_level='DEBUG')
 
 
 if __name__ == '__main__':
