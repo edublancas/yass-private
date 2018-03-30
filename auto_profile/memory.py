e0d5770ec0eadb91d6b915cb0fb04f31b3911cc7
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-03-29 21:54:07 -0400

    changes to memory profiling script

---
+++ auto_profile/memory.py
@@ -0,0 +1,44 @@
+"""
+Memory profiling. Looks for a config.yaml file in the same folder.
+
+See this for usage:
+
+https://github.com/pythonprofilers/memory_profiler
+
+Run with:
+
+python -m memory_profiler memory.py yass.preprocess.run
+
+or
+
+mprof run memory.py yass.preprocess.run
+
+To plot:
+
+mprof plot
+"""
+from pydoc import locate
+import argparse
+import logging
+import yass
+from yass import preprocess
+from memory_profiler import profile
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("function", type=str,
+                        help="Path to function to profile")
+    parser.add_argument("-l", "--logger", type=str, help="Logger level",
+                        default="INFO")
+    args = parser.parse_args()
+
+    logging.basicConfig(level=getattr(logging, args.logger))
+    yass.set_config('config.yaml')
+
+    function = locate(args.function)
+
+    fp=open('{}.log'.format(args.function.replace('.', '_')),'w+')
+    to_profile = profile(preprocess.run, stream=fp)
+    to_profile(if_file_exists='overwrite')
+    fp.close()
