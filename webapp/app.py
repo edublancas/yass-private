93bbabc376ad77ddf9bab3777aa127a20f758060
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-06-01 13:44:25 -0400

    flask app

---
+++ webapp/app.py
@@ -0,0 +1,20 @@
+from redis import Redis
+import rq
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+    app.redis = Redis.from_url('redis://localhost:6379')
+    app.task_queue = rq.Queue('performance-testing', connection=app.redis,
+                              default_timeout=3600)
+    return app
+
+
+app = create_app()
+
+
+@app.route('/yass/<git_hash>')
+def hello_world(git_hash):
+    job = app.task_queue.enqueue('tasks.performance_testing', 'testing')
+    return 'Testing yass@{}, job with id {}'.format(git_hash, job.get_id())
--- webapp/send_task.py
+++ webapp/send_task.py
@@ -2,7 +2,8 @@ from redis import Redis
 import rq
 
 queue = rq.Queue('performance-testing',
-                 connection=Redis.from_url('redis://localhost:6379'))
+                 connection=Redis.from_url('redis://localhost:6379'),
+                 default_timeout=3600)
 
 job = queue.enqueue('tasks.performance_testing', 'testing')
 
