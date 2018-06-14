f860c6e13b4d33e1b69100c91da0f981dd86df5e
Eduardo Blancas <fkq8@blancas.io>
2018-06-14 15:56:25 -0400

    keras implementation working

--- nnet/models.py
+++ nnet/models.py
@@ -3,24 +3,27 @@ https://keras.io/getting-started/sequential-model-guide/
 """
 
 from keras.models import Sequential
-from keras.layers import LSTM, Dense, Conv2D
+from keras.layers import LSTM, Dense, Conv2D, Flatten
 import numpy as np
 
 
-def default_model(x_train, input_shape, K1, K2):
+def default_model(x_train, input_shape):
     n_data, window_size, n_channels, _ = x_train.shape
 
     model = Sequential()
 
     model.add(Conv2D(16, kernel_size=(3, 3),
                      padding='valid', activation='relu', use_bias=True,
-                     input_shape=input_shape))
+                     input_shape=input_shape, data_format="channels_last"))
 
     model.add(Conv2D(16, kernel_size=(3, 3),
                      padding='same', activation='relu', use_bias=True))
 
     model.add(Conv2D(16, kernel_size=(3, 3),
                      padding='valid', activation='relu', use_bias=True))
+    
+    model.add(Flatten())
+    model.add(Dense(1, activation='softmax'))
 
     model.compile(loss='binary_crossentropy',
                   optimizer='adam',
