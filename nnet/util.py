0fdeabaa80c1a99a0c0a2b3d5723f26a309e5f30
Eduardo Blancas Reyes <fkq8@blancas.io>
2018-08-23 00:42:53 -0400

    removes old files in nnet

--- nnet/util.py
+++
@@ -1,7 +0,0 @@
-import datetime
-
-def directory():
-    """Generate a valid directory name with the current datetime
-    """
-    now = datetime.datetime.now()
-    return now.strftime('%d-%b-%Y@%H-%M-%S')
